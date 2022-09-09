98024e2feef529f2df9001a5f2b7383df23fbb3d
letow <quackgamer26@gmail.com>
2022-09-09 15:51:32 +0500

    infocontainer gap and popover btns changed

--- src/App.tsx
+++ src/App.tsx
@@ -65,7 +65,7 @@ const App: FC = () => {
                             {...a11yProps(1)}
                         />
                         <Tab
-                            label="Писец"
+                            label="Собесы"
                             {...a11yProps(2)}
                         />
                     </Tabs>
