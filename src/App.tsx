e81d2e310745e225cba57ff1f9fe2cd63ae486b7
George <71167099+letow@users.noreply.github.com>
2022-10-13 18:42:55 +0500

    redundant deps removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import "./App.css";
 import { FC, memo, useEffect } from "react";
-import "react-big-calendar/lib/css/react-big-calendar.css";
-import "@toast-ui/calendar/dist/toastui-calendar.min.css";
 import ReactBigCalendar from "./Components/ReactBigCalendar/ReactBigCalendar";
 import { useAppDispatch } from "./Redux/Hooks";
 import { setRoleAction } from "./Redux/Actions/SetRoleAction";
