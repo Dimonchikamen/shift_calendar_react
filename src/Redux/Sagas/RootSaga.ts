d214e2ce781801c31820f99cd1a3657c7c281d78
Dimonchikamen <65847130+Dimonchikamen@users.noreply.github.com>
2022-11-09 17:17:52 +0500

    change fork effect to call

--- src/Redux/Sagas/RootSaga.ts
+++ src/Redux/Sagas/RootSaga.ts
@@ -1,4 +1,4 @@
-import { all, fork } from "redux-saga/effects";
+import { all, fork, call } from "redux-saga/effects";
 import GetEventsSaga from "./EventsSagas/GetEventsSaga";
 import ChangeInterviewTimeSaga from "./InterviewTimeSagas/ChangeInterviewTimeSaga";
 import AddRecruiterWorkTimeSaga from "./RecruitersSagas/AddRecruiterWorkTimeSaga";
@@ -10,13 +10,13 @@ import SignUpVolunteerSaga from "./SignUpVolunteersaga/SignUpVolunteerSaga";
 
 export function* rootSaga() {
     yield all([
-        fork(GetEventsSaga),
-        fork(GetInformationSaga),
-        fork(ChangeWorkDaySaga),
-        fork(ChangeInterviewTimeSaga),
-        fork(AddRecruiterWorkTimeSaga),
-        fork(RemoveRecruiterWorkTimeSaga),
-        fork(EditRecruiterWorkTimeSaga),
-        fork(SignUpVolunteerSaga),
+        call(GetEventsSaga),
+        call(GetInformationSaga),
+        call(ChangeWorkDaySaga),
+        call(ChangeInterviewTimeSaga),
+        call(AddRecruiterWorkTimeSaga),
+        call(RemoveRecruiterWorkTimeSaga),
+        call(EditRecruiterWorkTimeSaga),
+        call(SignUpVolunteerSaga),
     ]);
 }
